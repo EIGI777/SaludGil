cf6c8428b564957de67e22f4923c6aecbaec4eca
EIGI777 <jceagb@gmail.com>
2025-07-24 17:26:06 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,17 @@
-// Configuración de Firebase
-const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_PROYECTO.firebaseapp.com",
-    projectId: "TU_PROYECTO",
-    storageBucket: "TU_PROYECTO.appspot.com",
-    messagingSenderId: "TU_SENDER_ID",
-    appId: "TU_APP_ID"
-};
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
 
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+  const firebaseConfig = {
+    apiKey: "AIzaSyBBzkIspTEddUQeiecXGU4-2y8N8vdOXxA",
+    authDomain: "saludgil-ad9f3.firebaseapp.com",
+    projectId: "saludgil-ad9f3",
+    storageBucket: "saludgil-ad9f3.firebasestorage.app",
+    messagingSenderId: "454890537120",
+    appId: "1:454890537120:web:e985a8bc9e79f63c3209c5",
+    measurementId: "G-ZLYYL9R44X"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const auth = firebase.auth();
